a2bfe221c2af73d20e042f784236cab336fb1b16
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2025-01-12 12:17:33 +0500

    ddd

--- src/app/components/Home/Products.tsx
+++ src/app/components/Home/Products.tsx
@@ -10,8 +10,7 @@ type prod = {
   imageUrl: string;
 };
 
-const ProductGrid = async() => {
-  
+const ProductGrid = async () => {
   const query = await client.fetch(`*[_type == "product"]{
     _id,
     name,
@@ -21,8 +20,7 @@ const ProductGrid = async() => {
     "imageUrl": image.asset->url
   }`);
 
- const products = query.slice(0, 4)
-
+  const products = query.slice(0, 4);
 
   return (
     <div className="bg-gray-50 py-12">
@@ -30,23 +28,23 @@ const ProductGrid = async() => {
         Featured Products
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
-        {products.map((product:prod, index:number) => (
-          <Link href={`/products/${product._id}`}>
-          <div className="group relative p-4 hover:bg-[#4c35ff] hover:text-white rounded-lg shadow hover:shadow-lg transition">
-            <div
-              className="flex items-center justify-center h-64 bg-[#F6F7FB] bg-cover bg-center bg-no-repeat"
-              style={{
-                backgroundImage: `url(${product.imageUrl})`,
-              }}
-            ></div>
-            <div className="absolute top-0 left-0 w-full h-full hover:text-white flex items-center justify-center group-hover:border-[#1A0B5B] border-2"></div>
-            <div className="mt-4 text-center hover:text-white">
-              <h3 className="text-lg font-medium cursor-pointer">{product.name}</h3>
-              <p className="text-sm text-gray-600 hover:text-white">Code - {product._id}</p>
-              <p className="text-lg font-bold text-gray-900 hover:text-white">{product.price}</p>
+        {products.map((product: prod) => (
+          <Link key={product._id} href={`/products/${product._id}`}>
+            <div className="group relative p-4 hover:bg-[#4c35ff] hover:text-white rounded-lg shadow hover:shadow-lg transition">
+              <div
+                className="flex items-center justify-center h-64 bg-[#F6F7FB] bg-cover bg-center bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${product.imageUrl})`,
+                }}
+              ></div>
+              <div className="absolute top-0 left-0 w-full h-full hover:text-white flex items-center justify-center group-hover:border-[#1A0B5B] border-2"></div>
+              <div className="mt-4 text-center hover:text-white">
+                <h3 className="text-lg font-medium cursor-pointer">{product.name}</h3>
+                <p className="text-sm text-gray-600 hover:text-white">Code - {product._id}</p>
+                <p className="text-lg font-bold text-gray-900 hover:text-white">{product.price}</p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
         ))}
       </div>
     </div>
